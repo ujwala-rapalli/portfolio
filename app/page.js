79368a7f13d95e3e31f0d5723204fdac404150f6
ujwala-rapalli <22211a6697@bvrit.ac.in>
2025-04-16 21:59:59 +0530

    added

--- app/page.js
+++ app/page.js
@@ -15,10 +15,10 @@ export default async function Home() {
     <div suppressHydrationWarning >
       <HeroSection />
       <AboutSection />
-      <Experience />
+      {/* <Experience /> */}
       <Skills />
       <Projects />
-      <Education />
+      {/* <Education /> */}
       <Blog />
       <ContactSection />
     </div>
